f9be504d8f613f56632f9c142c07e2640727cd18
BERK <guneybrk95@gmail.com>
2024-07-25 23:29:54 +0300

    Enemies ScriptableObjects

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Enemies",menuName ="Enemies")]
+public class Enemies : ScriptableObject 
+{
+    [SerializeField] string _name;
+    [SerializeField] float _healthPoints; 
+    public float HealthPoints => _healthPoints;
+
+    [SerializeField] float _speed;
+
+
+    [SerializeField] float _damagePoints;
+    public float DamagePoints => _damagePoints;
+
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerAudio), typeof(PlayerInput),typeof(PlayerMovement))]
+[RequireComponent(typeof(PlayerAudio), typeof(PlayerInput), typeof(PlayerMovement))]
 public class Player : MonoBehaviour
 {
     [SerializeField] Weapon _weapons;
     [SerializeField] PlayerMovement _playerMovement;
     [SerializeField] PlayerInput _playerInput;
     [SerializeField] PlayerAudio _playerAudio;
+
+    private void Start()
+    {
+        _playerAudio = GetComponent<PlayerAudio>();
+        _playerInput = GetComponent<PlayerInput>();
+        _playerMovement = GetComponent<PlayerMovement>();
+    }
 }
 public class PlayerAudio : MonoBehaviour
 {
-
+    
 }
 
 public class PlayerInput : MonoBehaviour
@@ -20,7 +25,7 @@ public class PlayerInput : MonoBehaviour
 
 }
 
-public class PlayerMovement: MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
 }
