76a8fe797760a5d5286e8751fea4f4bb61e7bd31
BERK <guneybrk95@gmail.com>
2024-07-25 22:15:11 +0300

    Weapon ScriptableObject and Player class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerAudio), typeof(PlayerInput),typeof(PlayerMovement))]
 public class Player : MonoBehaviour
 {
-    
+    [SerializeField] Weapon _weapons;
+    [SerializeField] PlayerMovement _playerMovement;
+    [SerializeField] PlayerInput _playerInput;
+    [SerializeField] PlayerAudio _playerAudio;
+}
+public class PlayerAudio : MonoBehaviour
+{
+
+}
+
+public class PlayerInput : MonoBehaviour
+{
+
+}
+
+public class PlayerMovement: MonoBehaviour
+{
+
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName ="Weapon", menuName ="Weapon")]
 public class Weapon : ScriptableObject
 {
-    string _name;
+    public string _name;
+    [SerializeField]
     float _damage;
+    public float BaseDamage => _damage;
+    [SerializeField, Min(0)]
     float _fireRate;
+    public float FireRate => _fireRate;
+    [SerializeField]
     float _reloadTime;
+    public float ReloadTime => _reloadTime;
 }
