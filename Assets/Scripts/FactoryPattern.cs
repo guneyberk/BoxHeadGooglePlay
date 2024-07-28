c10bca3f3083b2ab31c2bf5221b9553ca6d7a2bb
BERK <guneybrk95@gmail.com>
2024-07-28 17:37:48 +0300

    Standart Zombie FactoryPattern

--- Assets/Scripts/FactoryPattern.cs
+++ Assets/Scripts/FactoryPattern.cs
@@ -13,25 +13,16 @@ public abstract class Factory : MonoBehaviour
     public abstract IProduct GetProduct(Vector3 position);
 }
 
-public class Standartombie : MonoBehaviour, IProduct
-{
-    [SerializeField] private string productName = "ProductA";
-    public string ProductName { get => productName; set => productName = value;}
 
-    public void Initialize()
-    {
-        gameObject.name = productName;
-    }
-}
 
 public class ConcreateFactoryA: Factory
 {
-    [SerializeField] private ProductA productPrefab;
+    [SerializeField] private StandartZombie productPrefab;
 
     public override IProduct GetProduct(Vector3 position)
     {
         GameObject instance = Instantiate(productPrefab.gameObject,position,Quaternion.identity);
-        ProductA newProduct = instance.GetComponent<ProductA>();
+        StandartZombie newProduct = instance.GetComponent<StandartZombie>();
 
         newProduct.Initialize();
         return newProduct;
