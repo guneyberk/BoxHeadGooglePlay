0724e667027880dd03c15d2ce5ad7e48360e3468
BERK <guneybrk95@gmail.com>
2024-07-31 22:46:17 +0300

    ObjectPool

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Pool;
 
 public class ObjectPool : MonoBehaviour
 {
     [SerializeField] uint initPoolSize;
-    [SerializeField]  PooledObject objectToPool;
+    [SerializeField] PooledObject objectToPool;
 
-    private Stack<PooledObject> stack;  
-    // Start is called before the first frame update
+    private Stack<PooledObject> stack;
     void Start()
     {
         SetupPool();
@@ -24,7 +20,7 @@ public class ObjectPool : MonoBehaviour
         for (int i = 0; i < initPoolSize; i++)
         {
             instance = Instantiate(objectToPool);
-            instance.Pool= this;
+            instance.Pool = this;
             instance.gameObject.SetActive(false);
             stack.Push(instance);
         }
@@ -32,10 +28,10 @@ public class ObjectPool : MonoBehaviour
 
     public PooledObject GetPooledObject()
     {
-        if(stack.Count == 0)
+        if (stack.Count == 0)
         {
             PooledObject newInstance = Instantiate(objectToPool);
-            newInstance.Pool= this;
+            newInstance.Pool = this;
             return newInstance;
         }
 
@@ -43,7 +39,7 @@ public class ObjectPool : MonoBehaviour
         nextInstance.gameObject.SetActive(true);
         return nextInstance;
 
-       
+
     }
 
     public void ReturnToPool(PooledObject pooledObject)
@@ -54,14 +50,5 @@ public class ObjectPool : MonoBehaviour
     }
 
 
-    public class PooledObject : MonoBehaviour
-    {
-        private ObjectPool pool;
-        public ObjectPool Pool { get { return pool;} set { pool = value;} }
 
-        public void Release()
-        {
-            pool.ReturnToPool(this);
-        }
-    }
 }
---
+++ Assets/Scripts/PooledObject.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PooledObject : MonoBehaviour
+{
+
+    private ObjectPool pool;
+    public ObjectPool Pool { get { return pool; } set { pool = value; } }
+
+    public void Release()
+    {
+        pool.ReturnToPool(this);
+    }
+}
+
