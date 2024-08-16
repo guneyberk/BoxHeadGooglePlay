1f7d377c03ada8111ab54478bb5596d0097e08c1
BERK <guneybrk95@gmail.com>
2024-08-16 14:59:26 +0300

    command

--- Assets/Scripts/CommandPattern/CommandInvoker.cs
+++ Assets/Scripts/CommandPattern/CommandInvoker.cs
@@ -4,19 +4,5 @@ using UnityEngine;
 
 public class CommandInvoker : MonoBehaviour
 {
-    private static Stack<ICommand> undoStack = new Stack<ICommand>();
-    public static void ExecuteCommand(ICommand command)
-    {
-        command.Execute();
-        undoStack.Push(command);
-    }
-
-    public static void UndoCommand()
-    {
-        if (undoStack.Count > 0)
-        {
-            ICommand activeCommand = undoStack.Pop();
-            activeCommand.undo();
-        }
-    }
+   
 }
--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -4,29 +4,4 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
-    private ICommand _moveCommand;
-    private ICommand _shootCommand;
-
-    private Stack<ICommand> moveStack = new Stack<ICommand>();
-
-    public void SetCommands(ICommand moveCommand, ICommand shootCommand)
-    {
-        this._moveCommand = moveCommand;
-        this._shootCommand = shootCommand;
-    }
-
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            _moveCommand.Execute();
-            moveStack.Push(_moveCommand);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            _moveCommand.Execute();
-            moveStack.Push(_shootCommand);
-        }
-    }
 }
